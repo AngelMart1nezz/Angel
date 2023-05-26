7df41709be36d3f81b2f1317957adc3cd11df1b8
AngelMart1nez <anglmartnez7@gmail.com>
2023-05-25 23:59:54 -0700

    Cambie el HTML para el sitio.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
+from blog.forms import PostForm
 
-# Create your views here.
+def home(request):
+    form=PostForm()
+    return render(request,'blog/home.html',{'form':form})
